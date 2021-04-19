bd997418aed0c905f6ddf9fc9ea0856250bf87ea
superucitelka <dokonala.hilda@gmail.com>
2021-04-19 17:08:11 +0200

    05 - zaklad: list a detail

--- bazar/urls.py
+++ bazar/urls.py
@@ -4,4 +4,6 @@ from . import views
 # URL mapování - seznam URL adres pro aplikaci bazar
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list', views.BazarListView.as_view(), name='list'),
+    path('detail/<int:pk>/', views.BazarDetailView.as_view(), name='detail'),
 ]
--- bazar/views.py
+++ bazar/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from bazar.models import *
+from django.views.generic import ListView, DetailView
+
 
 def index(request):
     context = {
@@ -8,3 +11,17 @@ def index(request):
 
     return render(request, template_name='index.html', context=context)
 
+
+class BazarListView(ListView):
+    model = Zbozi
+
+    context_object_name = 'zbozi_list'
+    template_name = 'list.html'
+    #paginate_by = 3
+
+
+class BazarDetailView(DetailView):
+    model = Zbozi
+
+    context_object_name = 'zbozi'
+    template_name = 'detail.html'
